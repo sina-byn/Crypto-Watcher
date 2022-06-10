174a28c4c9751e318161e27ef2e8a861b0bffd3a
sina-byn <sina.sina.bayan@protonmail.com>
2022-06-10 20:35:08 +0430

    main: CoinCard color bug fixed

--- components/Homepage/CoinCard.tsx
+++ components/Homepage/CoinCard.tsx
@@ -1,12 +1,14 @@
-import { FC, useContext, useEffect, useState } from "react";
+import { FC, useContext, useEffect } from "react";
 
 // Importing Context + Interfaces
 import { AppContext } from "../../context/AppContextProvider";
-import { moveInOut } from "../../helpers/animations";
 import { AppCtx } from "../../interfaces/interfaces";
 
 // Importing Interfaces
 import { CoinProps } from "../../interfaces/interfaces";
+
+// Importing Functions
+import { moveInOut } from "../../helpers/animations";
 
 const CoinCard: FC<CoinProps> = (props) => {
   const {
@@ -19,23 +21,7 @@ const CoinCard: FC<CoinProps> = (props) => {
     rank,
     img,
   } = props;
-  const [cardTheme, setCardTheme] = useState<string>();
   const ctx: AppCtx | null = useContext<AppCtx | null>(AppContext);
-
-  useEffect(() => {
-    const setColor = (price_change: number | null | undefined): string => {
-      if (price_change) {
-        if (price_change > 0) {
-          return "success";
-        } else if (price_change < 0) {
-          return "error";
-        }
-      }
-      return "gray-500";
-    };
-
-    setCardTheme(setColor(price_change));
-  }, [cardTheme]);
 
   const clickHandler = (): void => {
     if (ctx) {
@@ -56,39 +42,67 @@ const CoinCard: FC<CoinProps> = (props) => {
     }
   };
 
-  return (
-    <div
-      className='card flex justify-end w-full h-20 relative overflow-hidden'
-      onClick={clickHandler}
-    >
+  const setColorStop = (): string => {
+    if (price_change) {
+      switch (price_change > 0) {
+        case true:
+          return "from-success";
+        default:
+          return "from-error";
+      }
+    }
+    return "from-gray-500";
+  };
+
+  const setTextColor = (): string => {
+    if (price_change) {
+      switch (price_change > 0) {
+        case true:
+          return "text-success";
+        default:
+          return "text-error";
+      }
+    }
+    return "text-gray-500";
+  };
+
+  if (price_change !== undefined && price_change !== null) {
+    return (
       <div
-        className={`card-design h-full w-1/2 absolute bg-gradient-to-l from-${cardTheme}`}
-      ></div>
-      <div className='card-data flex justify-between items-center gap-x-2 w-full h-full absolute bg-primary backdrop-blur-xl p-4'>
-        <figure className='grid place-items-center w-14 h-14 rounded-full'>
-          <img src={img} alt={name + "sign"} className='w-10' />
-        </figure>
-        <div className='flex flex-col flex-grow gap-y-1'>
-          <p className='text-gray-100'>
-            {symbol?.toUpperCase()}
-            <span className='text-xs text-gray-500'>/USDT</span>
-          </p>
-          <p className='text-gray-500 text-xs'>
-            Vol. {total_volume ? (total_volume / 1000000).toFixed(2) : "---"}M
-          </p>
-        </div>
-        <div className='flex flex-col text-md items-end gap-y-1'>
-          <p className={`text-${cardTheme}`}>
-            {price_change?.toLocaleString()}
-            <span className='text-xs'>%</span>
-          </p>
-          <p className='text-xs text-gray-500'>
-            ${current_price?.toLocaleString()}
-          </p>
+        className='card flex justify-end w-full h-20 relative overflow-hidden'
+        onClick={clickHandler}
+      >
+        <div
+          className={`card-design h-full w-1/2 absolute bg-gradient-to-l ${setColorStop()}`}
+        ></div>
+        <div className='card-data flex justify-between items-center gap-x-2 w-full h-full absolute bg-primary backdrop-blur-xl p-4'>
+          <figure className='grid place-items-center w-14 h-14 rounded-full'>
+            <img src={img} alt={name + "sign"} className='w-10' />
+          </figure>
+          <div className='flex flex-col flex-grow gap-y-1'>
+            <p className='text-gray-100'>
+              {symbol?.toUpperCase()}
+              <span className='text-xs text-gray-500'>/USDT</span>
+            </p>
+            <p className='text-gray-500 text-xs'>
+              Vol. {total_volume ? (total_volume / 1000000).toFixed(2) : "---"}M
+            </p>
+          </div>
+          <div className='flex flex-col text-md items-end gap-y-1'>
+            <p className={setTextColor()}>
+              {price_change?.toLocaleString()}
+              <span className='text-xs'>%</span>
+            </p>
+            <p className='text-xs text-gray-500'>
+              ${current_price?.toLocaleString()}
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return <p>Failed to Load data</p>;
 };
 
 export default CoinCard;
