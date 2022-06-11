7bad79da01116c8fb59c2221020db47d975645a5
sina-byn <sina.sina.bayan@protonmail.com>
2022-06-11 14:36:44 +0430

    main: Hydration Error due to p tag Misusage Handled

--- components/DeveloperInfo.tsx
+++ components/DeveloperInfo.tsx
@@ -9,18 +9,18 @@ const DeveloperInfo: FC = () => {
       </figure>
       <div className='flex flex-col gap-y-3 text-gray-500 mx-auto mt-6'>
         <p className='font-bold'>
-          Fullname:{" "}
+          Fullname:
           <span className='text-sm text-gray-200 font-light'>
             Sina Bayandorian
           </span>
         </p>
-        <p className='font-bold'>
+        <div className='font-bold'>
           Education:
           <p className=' text-sm text-gray-200 font-light'>
-            <p>Software Engineering Student at KIAU</p>
-            <p>Karaj Islmic Azad University</p>
+            <span className="block">Software Engineering Student at KIAU</span>
+            <span className="block">Karaj Islmic Azad University</span>
           </p>
-        </p>
+        </div>
         <div className='flex w-full gap-x-4 mt-6 mb-10'>
           <a
             className='w-full text-xs text-center text-gray-200 border-2 border-gray-200 py-2 mx-auto hover:bg-gray-100 hover:bg-opacity-25'
