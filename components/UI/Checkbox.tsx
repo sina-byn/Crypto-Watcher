d2f2237c72271ae2ce5fe33a151298e32e688f30
sina-byn <sina.sina.bayan@protonmail.com>
2022-06-11 18:13:57 +0430

    main: icons' files replaced with svgs

--- components/UI/Checkbox.tsx
+++ components/UI/Checkbox.tsx
@@ -44,8 +44,8 @@ const Checkbox: FC<Props> = ({ id }) => {
       if (!isSelected) {
         const unifiedSelectedCoins: string[] = Array.from(
           new Set([...selectedCoins, id])
-          );
-          setSelectedCoins(unifiedSelectedCoins);
+        );
+        setSelectedCoins(unifiedSelectedCoins);
       } else {
         if (isRemovable) {
           const coinIDX: number = selectedCoins.indexOf(id);
@@ -66,11 +66,14 @@ const Checkbox: FC<Props> = ({ id }) => {
     >
       <div className='w-5 h-5 bg-gray-100 rounded-full cursor-pointer ml-2'>
         {isSelected ? (
-          <img
-            src='/icons/circle-check.svg'
-            alt='Circle Check Icon'
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 512 512'
+            fill='#00ba00'
             className='cursor-pointer'
-          />
+          >
+            <path d='M0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256zM371.8 211.8C382.7 200.9 382.7 183.1 371.8 172.2C360.9 161.3 343.1 161.3 332.2 172.2L224 280.4L179.8 236.2C168.9 225.3 151.1 225.3 140.2 236.2C129.3 247.1 129.3 264.9 140.2 275.8L204.2 339.8C215.1 350.7 232.9 350.7 243.8 339.8L371.8 211.8z' />
+          </svg>
         ) : (
           ""
         )}
--- components/UI/ScrollTopButton.tsx
+++ components/UI/ScrollTopButton.tsx
@@ -23,10 +23,12 @@ const ScrollTopButton: FC = () => {
       type='button'
       className={`${
         !isShown || isModalShown ? "hidden" : "initial"
-      } w-8 h-8 test sticky bottom-12 z-40 bg-gray-100 rounded-full px-2 ml-4 md:-ml-12`}
+      } w-8 h-8 sticky bottom-12 z-40 bg-gray-100 rounded-full px-2 ml-4 md:-ml-12`}
       onClick={clickHandler}
     >
-      <img src='/icons/chevron-up.svg' alt='Chevron Top Icon' />
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 448 512'>
+        <path d='M416 352c-8.188 0-16.38-3.125-22.62-9.375L224 173.3l-169.4 169.4c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25l192-192c12.5-12.5 32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25C432.4 348.9 424.2 352 416 352z' />
+      </svg>
     </button>
   );
 };
