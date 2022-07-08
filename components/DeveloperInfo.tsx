442c4b58fd9d86387e855c5845ce14083b2fd352
sina-byn <sina.sina.bayan@protonmail.com>
2022-07-08 11:28:30 +0430

    main: DeveloperInfo updated

--- components/DeveloperInfo.tsx
+++ components/DeveloperInfo.tsx
@@ -36,16 +36,16 @@ const DeveloperInfo: FC = () => {
         </div>
         <div className='flex w-full gap-x-4 mt-6 mb-10'>
           <a
-            className='w-full text-xs text-center text-gray-200 border-2 border-gray-200 py-2 mx-auto hover:bg-gray-100 hover:bg-opacity-25'
-            href='https://sina-byn.github.io/My-Portfolio/about-me.html'
+            className='w-full text-xs text-center text-gray-200 border-2 border-gray-200 pt-2 pb-[0.7rem] mx-auto hover:bg-gray-100 hover:bg-opacity-25'
+            href='https://sina-bayan.netlify.app/'
             target='_blank'
             rel="noreferrer"
           >
-            Portfolio
+            Website
           </a>
           <a
-            className='w-full text-xs text-center text-gray-200 border-2 border-gray-200 py-2 mx-auto hover:bg-gray-100 hover:bg-opacity-25'
-            href='/sina-bayandorian.pdf'
+            className='w-full text-xs text-center text-gray-200 border-2 border-gray-200 pt-2 pb-[0.7rem] mx-auto hover:bg-gray-100 hover:bg-opacity-25'
+            href='https://sina-bayan.netlify.app/uploads/sina-bayandorian.pdf'
             download
           >
             Resume
